cf00e763370262ce63941b25aa15a2aa881650cb
Hyazid <yazid.hanniche.me@gmail.com>
2025-01-27 14:28:37 +0000

    ch11

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book,Review
 # Register your models here.
 
-
+class ReviewInline(admin.TabularInline):
+    model = Review
 class BookAdmin(admin.ModelAdmin):#specify wich field we also want to display
     list_display = ('title', 'author', 'price')
+    inlines = [
+        ReviewInline,  # Display the reviews inline with the book
+    ]
 admin.site.register(Book,BookAdmin)
 
