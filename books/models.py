3b186fe0b6b7e7bd5682a88b576c79ba75045406
Hyazid <yazid.hanniche.me@gmail.com>
2025-02-03 13:35:14 +0000

    ch14

--- books/models.py
+++ books/models.py
@@ -10,6 +10,10 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     price=models.DecimalField(max_digits=6,decimal_places=2)
     cover=models.ImageField(upload_to='cover/', blank=True)
+    class Meta:
+        permissions=[
+            ('special_status','can read all books')
+        ]
     def __str__(self):
         return self.title
     def get_absolute_url(self): # new
--- books/views.py
+++ books/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 from .models import Book
 from django.views.generic import ListView,DetailView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 
 class BookListView(ListView):
     model = Book
     context_object_name='book_list'
     template_name = 'books/book_list.html'
+    login_url='account_login'
 # Create your views here.
 
 class BookDetailView(DetailView):
     model = Book
     context_object_name='book'
     template_name='books/book_detail.html'
+    login_url='account_login'
+
+class SearchResultsListView(ListView):
+    model = Book
+    template_name = 'books/search_results.html'
+    context_object_name = 'book_list'
+
+class SearchResultsListView(ListView):
+    model = Book
+    template_name = 'books/search_results.html'
+    context_object_name = 'book_list'
+    def get_queryset(self):
+        query= self.request.GET.get('q')
+        return Book.objects.filter(Q(title__icontains=query)|Q(author__icontains=query))
